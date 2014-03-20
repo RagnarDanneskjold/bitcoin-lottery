8cea6980bd2020ccc63450d16bb81bf3c43f463b
Joshua Beckwith <josh.beckwith@gmail.com>
2014-03-19 17:23:10 -0700

    chance to win displays after asking how many tix are owned by an address

--- site/js/main.js
+++ site/js/main.js
@@ -24,16 +24,12 @@ var app = {
 	model:{},
 	render:function(){
 		document.body.innerHTML = this.template(this.model);
-		console.log(this)
 
 		var qrAnchor = document.querySelector('#address-container .qr-code');
 		var qrcode = new QRCode(qrAnchor, {
 		    text: 'bitcoin:' + this.model.address,
 		    width: 200,
 		    height: 200
-		    // colorDark : "#000000",
-		    // colorLight : "#ffffff",
-		    // correctLevel : QRCode.CorrectLevel.H
 		});
 
 		new Ticker(document.querySelector('.ticker-container'));
@@ -43,10 +39,11 @@ var app = {
 		$ticketQueryForm.submit(function(e){
 			e.preventDefault();
 			var addr = $addrInput[0].innerHTML;
-			console.log(addr);
 			$.get('api/ticketsByAddress?addr=' + addr).done(function(numSatoshi){
-				console.log(numSatoshi)
-				$ticketQueryForm.find('.result')[0].innerHTML = parseInt(numSatoshi) / 100000 + ' tickets';
+				var tix = parseInt(numSatoshi) / 100000;
+				console.log('tix', tix);
+				var chance = tix / Math.floor(app.model.final_balance / 100000) * 100;
+				$ticketQueryForm.find('.result')[0].innerHTML = tix + ' tickets. ' + chance + '% chance to win';
 			});
 		})
 
